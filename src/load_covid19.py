e06e50f922bfd46e70cf0d3b2db1bf1a798f6d4e
philipp <sproink@gmx.de>
2024-10-27 17:38:55 +0100

    Revert notebook/0.02 src/lload__covid19.py and src/transform.py to previous version

--- src/load_covid19.py
+++ src/load_covid19.py
@@ -1,24 +1,23 @@
 # load_covid19.py
 
 import os
+from src.data_downloader import download_covid19_dataset
 import pandas as pd
 
 # Use __file__ to get the directory of the current script
 current_dir = os.path.dirname(__file__)
 
-def load_covid19(download=True):
-    if download:
-        from src.data_downloader import download_covid19_dataset
-        download_covid19_dataset()  
+def load_covid19():
+    download_covid19_dataset()  
     
     file_path = os.path.join(os.path.dirname(current_dir), "data", "raw", "covid19-dataset", "Covid Data.csv")
     df = pd.read_csv(file_path)
     
     return df
 
-def load_clean_covid19(download=True):
+def load_clean_covid19():
     from src.transform import clean_covid19
-    clean_covid19(download)
+    clean_covid19()
 
     file_path = os.path.join(os.path.dirname(current_dir), "data", "interim", "covid-data-clean.csv")
     print(f"Loading clean dataset from: {file_path}")  # Debugging output
