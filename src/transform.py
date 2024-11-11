a9a538f0c7bc524a77655c93f9e231958b6392f0
RafaJC <rafajc@example.com>
2024-11-11 11:55:52 +0100

    Data filling in transform. Rerun examples.

--- src/transform.py
+++ src/transform.py
@@ -15,20 +15,20 @@ def clean_covid19():
                     'HIPERTENSION', 'CARDIOVASCULAR', 'RENAL_CHRONIC', 'OTHER_DISEASE', 'OBESITY', 'TOBACCO', 
                     'INTUBED', 'ICU']
 
-    missing_values = [97, 99]
+    missing_values = [97, 98, 99]
 
     # Convert Boolean columns to 'bool' and map values (Yes -> 1, No -> 0)
     for col in bool_columns:
         df[col] = df[col].map({1: 1, 2: 0}).astype('boolean')
     df.replace({col: missing_values for col in bool_columns if col in df.columns}, pd.NA, inplace=True)
 
-    df['SEX'] = df['SEX'].map({1: 'female', 2: 'male'})
+    # df['SEX'] = df['SEX'].map({1: 'female', 2: 'male'})
     df.replace('SEX', pd.NA, inplace=True)
     df['SEX'] = df['SEX'].astype('category')
 
-    df['PATIENT_TYPE'] = df['PATIENT_TYPE'].map({1: 'returned home', 2: 'hospitalization'})
-    df.replace('PATIENT_TYPE', pd.NA, inplace=True)
-    df['PATIENT_TYPE'] = df['PATIENT_TYPE'].astype('category')
+    # df['PATIENT_TYPE'] = df['PATIENT_TYPE'].map({1: 'athome', 2: 'hospitalization'})
+    # df.replace('PATIENT_TYPE', pd.NA, inplace=True)
+    # df['PATIENT_TYPE'] = df['PATIENT_TYPE'].astype('category')
 
     # DATE_DIED column alive value is '9999-99-99'
     df.DATE_DIED = df.DATE_DIED.apply(lambda x: 0 if x == "9999-99-99" else 1)
@@ -38,7 +38,30 @@ def clean_covid19():
 
     # Drop the original DATE_DIED column
     df.drop('DATE_DIED', axis=1, inplace=True)
-        
+    
+    # --------------------------------------
+    # Fix missing values RC, CK
+    
+    # Scale the age column using normalization
+    # This technique preserves the distribution and shifts values to a 0 to 1 scale
+    df['AGE'] = (df['AGE'] - df['AGE'].min()) / (df['AGE'].max() - df['AGE'].min())
+
+    # male cannot be pregnant
+    df.loc[df.SEX == 'male', 'PREGNANT'] = False
+    # zuhause kann nicht beatmet werde & intubiert werden
+    df.loc[df.PATIENT_TYPE==1,'ICU']=False
+    df.loc[df.PATIENT_TYPE==1,'INTUBED']=False
+    
+    # Drop rows with NaN values
+    df.dropna(axis=0, inplace=True)
+    
+    # --------------------------------------
+
+
+
+
+
+    
     # Define the file path and ensure the directory exists
     file_dir = os.path.join(os.path.dirname(current_dir), "data", "interim")
     os.makedirs(file_dir, exist_ok=True)  # Create directory if it doesn't exist
